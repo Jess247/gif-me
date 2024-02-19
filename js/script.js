37bd00e6732b67a9a82d5b805ade0c5469f92b61
Jess247 <jesss8692@hotmail.com>
2024-02-19 20:44:18 +0100

    Fetch gifs form api using users keyword
    Render gifs in grid

--- js/script.js
+++ js/script.js
@@ -1,9 +1,37 @@
-fetch('https://api.giphy.com/v1/gifs/search?api_key=dXEg9tx5ZJvJFMyxlI6ocE8zXoMEiQsi&q=cat&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips')
+
+const form = document.querySelector('#form')
+const searchfield = document.querySelector('#searchfield')
+
+form.addEventListener('submit', renderGifs)
+
+
+    /* 
+        user enters key word
+        on submit
+        add keyword to api url
+        loop through all objects 
+            create img tag 
+            add src
+            add class
+            append
+    */
+
+function renderGifs(e) {
+    e.preventDefault()
+    const keyword = searchfield.value
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=dXEg9tx5ZJvJFMyxlI6ocE8zXoMEiQsi&q=${keyword}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
     .then(response => response.json())
     .then(data => {
-        // const imgSrc = data.data[0].images.original.url
-        // const gif = document.createElement('img')
-        // gif.src = imgSrc
-        // console.log(gif)
-        // document.querySelector('#gifs').appendChild(gif)
+        const objLen = Object.keys(data.data).length
+
+        for (let i = 0; i < objLen; i++) {
+           document.querySelector('#gifs').innerHTML += `
+            <div class="gifs_container">
+                    <img src="${data.data[i].images.original.url}" alt="">
+                </div>
+            `
+        }
     })
+    searchfield.value = ''
+
+}
